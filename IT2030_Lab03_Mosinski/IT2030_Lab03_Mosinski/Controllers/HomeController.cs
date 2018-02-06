e74cf802ddf512625235a5170ce663c551821dcf
mosnick47 <mosnick47@gmail.com>
2018-02-05 21:48:33 -0500

    IT2030_Lab3_Mosinski

---
+++ IT2030_Lab03_Mosinski/IT2030_Lab03_Mosinski/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT2030_Lab02_Mosinski.Controllers
+{
+    public class HomeController : Controller    // The name of the controller exists right the .com/"" in the URL EX:facebook.com/home - home is the controller
+    {
+        public ActionResult Index()
+        {
+            return View("../Other/Details");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About method is being called.";
+
+            return View();
+        }
+
+        public string Display()
+        {
+            return "Display method is being called";
+        }
+    }
+}
---
+++ IT2030_Lab03_Mosinski/IT2030_Lab03_Mosinski/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT2030_Lab02_Mosinski.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Nicholas Mosinski 1/29/2018
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is display";
+        }
+
+        // /Products/Browse
+        public string Browse()
+        {
+            return "Browse displayed.";
+        }
+
+        // Products/Details/105
+        public string Details(int id)
+        {
+            return "Details displayed for Id=105";
+        }
+
+        // Products/Location?zip=44124
+        public string Locations(string zip)
+        {
+            return "Location displayed for zip=44142";
+        }
+    }
+}
