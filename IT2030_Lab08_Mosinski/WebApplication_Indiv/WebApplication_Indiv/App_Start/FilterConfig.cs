53097b5f4c2e9c64457c607ded383dcdeba1ca19
Nicholas Mosinski <mosnick47@gmail.com>
2018-03-12 19:38:35 -0400

    IT2030_Lab08_Mosinski

---
+++ IT2030_Lab08_Mosinski/WebApplication_Indiv/WebApplication_Indiv/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_Indiv
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new AuthorizeAttribute());  // indicates that all controllers need an authorization attribute; sets it at the global level
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ IT2030_Lab08_Mosinski/WebApplication_Indiv/WebApplication_Indiv/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_Indiv.Controllers
+{
+    [Authorize]     // Setting "[Authorize]" here will require login to use any of the controller methods
+    public class HomeController : Controller
+    {
+        //[AllowAnonymous]    // If we set global authorization in the FilterConfig.cs file, we need to add [AllowAnonymous] to any action methods that we may want to be the exception to the rule.    
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        // [Authorize]      // Will require the user to login to view the contact page
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
