61a7174e7ca7e1311c539a535a0e62e159896f63
Nicholas Mosinski <mosnick47@gmail.com>
2018-04-03 10:06:45 -0400

    IT2030_Lab10_Mosinski

---
+++ IT2030_Lab10_Mosinski/IT2030_Lab10_Mosinski_MusicStore/IT2030_Lab10_Mosinski_MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT2030_Lab04_Mosinski_MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddToCart(int? id)
+        {
+            return View();
+        }
+    }
+}
